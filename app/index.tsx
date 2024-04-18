31d9f7e1d3496d362fea3f04a3b9a3a1108d43bf
Reid Brown <reidbrown23@gmail.com>
2024-04-17 23:24:08 -0400

    Updates

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,53 @@
-import { Stack, Link } from 'expo-router';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
+import { Link } from 'expo-router';
 
 export default function Home() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
+      <View style={styles.header}>{/* You can add header content here */}</View>
+
+      <View style={styles.content}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={styles.title}>Home</Text>
+          <Link href={{ pathname: '/details', params: { name: 'Reid' } }} asChild>
+            <Text style={styles.button}>Go to Details</Text>
+          </Link>
+        </View>
+      </View>
+
+      <View style={styles.navbar}></View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    height: '10%',
+    backgroundColor: 'black', // Example color
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    height: '80%',
+    backgroundColor: '#ffffff', // Example color
+  },
+  navbar: {
+    height: '10%',
+    backgroundColor: 'black', // Example color
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: 'blue',
+    color: 'white',
+    borderRadius: 5,
+  },
+});
